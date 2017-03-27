4b1b967ebf488a48365a96e183162e014c73f8cb
mohong <arthas_qin@qq.com>
2017-03-27 22:15:58 +0800

    add Length

---
+++ Length.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.math.*;
+public class Length {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            double x1 = in.nextDouble();
+            double y1 = in.nextDouble();
+            double x2 = in.nextDouble();
+            double y2 = in.nextDouble();
+            double x = x1 - x2;
+            double y = y1 - y2;
+            double len = Math.sqrt(x * x + y * y);
+            System.out.printf("%.2f", len);
+            System.out.println();
+        }
+    }
+}
