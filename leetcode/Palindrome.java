c164d897e1224e6887ba7570cf9364f31f373926
mohong <arthas_qin@qq.com>
2017-07-06 22:15:43 +0800

    update

---
+++ leetcode/Palindrome.java
@@ -0,0 +1,17 @@
+package leetcode;
+// https://leetcode.com/problems/palindrome-number/#/description
+public class Palindrome {
+    public static boolean isPalindrome(int x) {
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int rev = 0;
+        while (x>rev){
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        return (x==rev || x==rev/10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(12321));
+    }
+}
